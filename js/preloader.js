8eb6d7b5f6b2278c3f9c45467196caf59c0d10ce
Lucas Oliveira <lucas.oliveira@rbstv.com.br>
2016-02-22 17:39:19 -0300

    Optimisation

--- js/preloader.js
+++ js/preloader.js
@@ -1,14 +1,1 @@
-
-
-(function preloader(){
-    image = new ImageLoader();
-    for(var i=0; i < gallery.length; i++){
-        image.load("/images/gallery/"+gallery[i][0]+".png");
-    }
-    image.onload(function () {
-        image.clearImages();
-        $('#preloader').fadeOut('slow', function () {
-            $(this).remove();
-        });
-    });
-})();
+(function preloader(){image=new ImageLoader();for(var i=0;i<gallery.length;i++){image.load("/images/gallery/"+gallery[i][0]+".png");}image.onload(function(){image.clearImages();$('#preloader').fadeOut('slow',function(){$(this).remove();});});})();
